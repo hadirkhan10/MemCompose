d6f4a4392300dd36c0dd71067241a19041fe9ffc
hadirkhan10 <hadirkhan10@gmail.com>
2022-11-27 17:31:07 -0800

    changed the structure to add separate functions for getting memory data and port data

--- parser.py
+++ parser.py
@@ -12,20 +12,56 @@ from pyverilog.dataflow.optimizer import VerilogDataflowOptimizer
 from pyverilog.dataflow.walker import VerilogDataflowWalker
 
 
+
 def parse_verilog(filelist, preprocess_include, preprocess_define):
     ast, directives = parse(filelist,
                             preprocess_include=preprocess_include,
                             preprocess_define=preprocess_define)
 
+    return ast
+
+    #mem_data = {}
+
+    #for child in ast.children():
+    #    # child.definitions gives us all the modules defined in the file
+    #    for module in child.definitions:
+    #        module_name = module.name
+    #        print(type(module.portlist))
+    #        for ports in module.portlist.ports:
+    #            #ports_data[ports.first.name] = (por
+    #            print(f"port type {ports.first}")
+    #            print(f"port name {ports.first.name}")
+    #            print(f"port width {ports.first.width}")
+    #        # module.items give us the delcarations, always and assign statements
+    #        for items in module.items:
+    #            # filtering out just the declarations
+    #            if str(type(items)) == "<class 'pyverilog.vparser.ast.Decl'>":
+    #                # items.list for the declarations give us the either Reg or wires that are delcared
+    #                for decl in items.list:
+    #                    # looking for just register definitions
+    #                    if str(type(decl)) == "<class 'pyverilog.vparser.ast.Reg'>":
+    #                        # filtering just the multidimensional register declarations (considered as memories)
+    #                        if (decl.dimensions != None):
+    #                            memory_name = decl.name
+    #                            mem_width = (int(decl.width.msb.value) - int(decl.width.lsb.value)) + 1
+    #                            for dimensions in decl.dimensions.lengths:
+    #                                mem_depth = (int(dimensions.msb.value) - int(dimensions.lsb.value)) + 1
+    #                                mem_data[module_name + '.' + memory_name] = (mem_depth, mem_width)
 
 
 
+
+    #ast.show()
+    #return mem_data
+
+def get_mem_data(ast):
     mem_data = {}
+
     for child in ast.children():
         # child.definitions gives us all the modules defined in the file
         for module in child.definitions:
-            # module.items give us the param list, port list, delcarations, always and assign statements
             module_name = module.name
+            # module.items give us the delcarations, always and assign statements
             for items in module.items:
                 # filtering out just the declarations
                 if str(type(items)) == "<class 'pyverilog.vparser.ast.Decl'>":
@@ -41,11 +77,13 @@ def parse_verilog(filelist, preprocess_include, preprocess_define):
                                     mem_depth = (int(dimensions.msb.value) - int(dimensions.lsb.value)) + 1
                                     mem_data[module_name + '.' + memory_name] = (mem_depth, mem_width)
 
-
-
-
-    #ast.show()
     return mem_data
+
+def get_ports(ast):
+    for child in ast.children():
+        # child.definitions gives us all the modules defined in the file
+        for module in child.definitions:
+            return module.portlist
 
 
 if __name__ == '__main__':
